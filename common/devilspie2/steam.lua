8716302ca5bc09619ea365a60200751400e97788
andreic92 <andreic9203@gmail.com>
2019-01-18 21:09:55 +0200

    some tweaks for my new arch installation and some old debts (lua)

---
+++ common/devilspie2/steam.lua
@@ -0,0 +1,15 @@
+if (string.match(get_window_name(), "Steam")) then
+   maximize();
+   set_window_workspace(4);
+   change_workspace(4);
+end
+if (string.match(get_window_name(), "Dota 2")) then
+   maximize();
+   set_window_workspace(4);
+   change_workspace(4);
+end
+if (string.match(get_window_name(), "Friends List")) then
+   maximize();
+   set_window_workspace(4);
+   change_workspace(4);
+end
