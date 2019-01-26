49578f3e239dc4c6c2a35f6ee42304541bb2a930
andreic92 <andreic92@gmail.com>
2019-01-26 18:33:17 +0200

    small fixes

---
+++ install_arch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+current_system=$(uname -s)
+configbuddy_url=$(curl -s  -i https://api.github.com/repos/andreic92/configbuddy.v2/releases/latest | grep download_url | grep tar.gz | grep -i $current_system | sed 's/": /#/' | cut -d'#' -f2 | sed 's/"//g')
+
+cd $HOME
+mkdir __delete_me_configbuddy
+cd __delete_me_configbuddy
+
+wget $configbuddy_url -O configbuddy.tar.gz
+tar -xf configbuddy.tar.gz
+
+mv configbuddy.v2 $HOME
+cd $HOME
+
+echo -n "Please provide what kind of computer do you configure: (work/home) "
+read configured_system
+./configbuddy.v2 -c $HOME/.dotfiles/configs/$configured_system.yml -c $HOME/.dotfiles/configs/i3.yml -b -p $HOME/bak
